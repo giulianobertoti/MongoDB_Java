2d437c2d67b8afcfa4094e0536be7a3a8a1c77f4
Giuliano Araujo Bertoti <giuliano.bertoti@gmail.com>
2022-03-18 19:28:12 -0300

    Update Model.java

--- Geo/src/main/java/hello/Model.java
+++ Geo/src/main/java/hello/Model.java
@@ -2,7 +2,7 @@ package hello;
 
 import java.util.*;
 import org.bson.Document;
-//import com.github.fakemongo.Fongo;
+import com.github.fakemongo.Fongo;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import static com.mongodb.client.model.Aggregates.*;
@@ -22,22 +22,22 @@ import com.mongodb.MongoClientURI;
 
 public class Model {
 	
-	MongoClientURI uri = new MongoClientURI("mongodb://localhost:27017/lab3"); 
-    MongoClient client = new MongoClient();
+	//MongoClientURI uri = new MongoClientURI("mongodb://localhost:27017/lab3"); 
+        //MongoClient client = new MongoClient();
     
-	//Fongo fongo = new Fongo("Mongo");
+	Fongo fongo = new Fongo("Mongo");
 	
 	public void addDocuments(Document doc){
-		MongoDatabase db = client.getDatabase(uri.getDatabase());
-		//MongoDatabase db = fongo.getDatabase("inpe");
+		//MongoDatabase db = client.getDatabase(uri.getDatabase());
+		MongoDatabase db = fongo.getDatabase("inpe");
 		MongoCollection<Document> researches = db.getCollection("researches");
     	researches.insertOne(doc);
 	}
 	
       
 	public Document searchByResearchYearCity(String nome, String ano, String geocodigo) {
-		MongoDatabase db = client.getDatabase(uri.getDatabase());
-		//MongoDatabase db = fongo.getDatabase("inpe");
+		//MongoDatabase db = client.getDatabase(uri.getDatabase());
+		MongoDatabase db = fongo.getDatabase("inpe");
     	MongoCollection<Document> researches = db.getCollection("researches");
 		   
 		Document doc = researches.aggregate(Arrays.asList(unwind("$municipio"),
@@ -49,8 +49,8 @@ public class Model {
 	
    
     public List<Document> searchByResearchYearCities(String nome, String ano, String[] geocodigo) {
-    	MongoDatabase db = client.getDatabase(uri.getDatabase());
-	   //MongoDatabase db = fongo.getDatabase("app");
+    	   //MongoDatabase db = client.getDatabase(uri.getDatabase());
+	   MongoDatabase db = fongo.getDatabase("app");
 	   MongoCollection<Document> collection = db.getCollection("users");
 	   List<Document> docs = new LinkedList<Document>();
 
